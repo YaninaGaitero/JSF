18d37786eef7938226f116f32f480d8fa031f713
brunomontoya17 <brucemontosh17@gmail.com>
2014-12-08 01:24:49 -0300

    Primera Parte Piqueos

--- src/java/Controladora/controladoraPiqueos.java
+++ src/java/Controladora/controladoraPiqueos.java
@@ -9,6 +9,10 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import BD.*;
 import Beans.*;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author usuario
@@ -19,8 +23,8 @@ public class controladoraPiqueos
 {
 
     private DaoPiqueo ConexionPiqueos;
-    
-    
+    private DaoFacturas ConexionCompras;
+    private ArrayList<Integer> selectedCompras= new ArrayList();
     /**
      * Creates a new instance of controladoraPiqueos
      * @throws java.lang.Exception
@@ -28,7 +32,35 @@ public class controladoraPiqueos
     public controladoraPiqueos() throws Exception 
     {
         ConexionPiqueos = new DaoPiqueo();
+        ConexionCompras = new DaoFacturas();
     }
     
+    public ArrayList<Compra> listaComprasPend()
+    {
+        try {
+            ArrayList<Compra> compras = new ArrayList();
+            Enumeration elist = ConexionCompras.TraerCompras().elements();
+            while (elist.hasMoreElements())
+            {
+                Compra pend = (Compra)elist.nextElement();
+                if (pend.getEstado()==1)
+                    compras.add(pend);
+            }
+            return compras;
+        } catch (Exception ex) {
+            Logger.getLogger(controladoraPiqueos.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    public ArrayList<Integer> getSelectedCompras() {
+        return selectedCompras;
+    }
+
+    public void setSelectedCompras(ArrayList<Integer> selectedCompras) {
+        this.selectedCompras = selectedCompras;
+    }
+
+
     
 }
